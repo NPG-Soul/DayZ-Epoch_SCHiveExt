be1d19c0ee7f6e0d9d410bba783c9d1f2b050756
UK431-Soul <dieuwer@gmail.com>
2014-08-13 11:36:43 +0200

    SCDBQuery2
    Transfers from morality to banksaldo

---
+++ SCDBQuery2.sql
@@ -0,0 +1,8 @@
+/* Transfers players bank money from morality fiedld in player_data to banksaldo in banking_data */
+
+INSERT IGNORE INTO banking_data 
+(PlayerUID, PlayerName)
+(SELECT PlayerUID, PlayerName FROM player_data);
+
+UPDATE banking_data
+SET BankSaldo = (SELECT PlayerMorality FROM player_data WHERE PlayerUID = banking_data.PlayerUID);
