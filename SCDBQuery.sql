5be7a46b054b4eff53776cd4d6bf424544b61492
UK431-Soul <dieuwer@gmail.com>
2014-08-17 00:20:37 +0200

    sql file update
    added lastupdated field to table

--- SCDBQuery.sql
+++ SCDBQuery.sql
@@ -9,6 +9,7 @@ CREATE TABLE `banking_data` (
   `PlayerUID` varchar(20) NOT NULL DEFAULT '0',
   `PlayerName` varchar(128) NOT NULL DEFAULT 'Null',
   `BankSaldo` bigint(24) NOT NULL DEFAULT '0',
+  `LastUpdated` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`PlayerUID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
